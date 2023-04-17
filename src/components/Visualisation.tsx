ce8f614ef4de7505dc985e5eceac92de936a3ae0
Joey Gough <j.gough@novafutur.com>
2023-04-17 23:41:59 +0100

    add resolution selector

--- src/components/Visualisation.tsx
+++ src/components/Visualisation.tsx
@@ -10,9 +10,6 @@ type VisualisationProps = {};
  * y axis is the imaginary part of the complex number
  */
 
-const xResolution = 0.01;
-const yResolution = 0.01;
-
 const mandelbrot = (c: { r: number; i: number }) => {
   let z = { x: 0, y: 0 };
   let n = 0;
@@ -30,23 +27,46 @@ const mandelbrot = (c: { r: number; i: number }) => {
 
 const colorScale = scaleLinear().domain([0, 100]).range([0, 360]);
 
+const initialXExtent = [-2.1, 0.7];
+const initialYExtent = [-1.2, 1.2];
+// const initialXExtent = [-1.9, 0.2];
+// const initialYExtent = [-1.2, 0];
+
+// const xResolution = 0.01;
+// const yResolution = 0.01;
+
+const resolutions = {
+  low: 100,
+  medium: 200,
+  high: 500,
+  "very high": 700,
+  "too high": 1000,
+} as const;
+
 export const Visualisation = (_props: VisualisationProps) => {
   const tooltipId = useId();
+  const [isDragging, setIsDragging] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
   const [svg, setSvg] = useState<null | SVGSVGElement>(null);
+  const [resolution, setResolution] = useState<keyof typeof resolutions>("low");
 
-  const [yExtent, setYExtent] = useState([-1.2, 1.2]);
-  const [xExtent, setXExtent] = useState([-2.1, 0.7]);
-  // const [yExtent, setYExtent] = useState([-1.2, 0]);
-  // const [xExtent, setXExtent] = useState([-1.9, 0.2]);
+  const [yExtent, setYExtent] = useState(initialYExtent);
+  const [xExtent, setXExtent] = useState(initialXExtent);
 
   const width = window.innerWidth;
   const height = window.innerHeight;
+
+  const desiredXPixels = Math.min(resolutions[resolution], width);
+  const desiredYPixels = Math.min(resolutions[resolution], height);
+  const xResolution = (xExtent[1] - xExtent[0]) / desiredXPixels;
+  const yResolution = (yExtent[1] - yExtent[0]) / desiredYPixels;
+
   useEffect(() => {
     if (!ref.current) return;
-    // delete the children
-    while (ref.current.firstChild) {
-      ref.current.removeChild(ref.current.firstChild);
+    // delete the svg if it exists
+    const existingSvg = ref.current.querySelector("svg");
+    if (existingSvg) {
+      existingSvg.remove();
     }
     // append svg
     const newSvg = document.createElementNS(
@@ -122,10 +142,75 @@ export const Visualisation = (_props: VisualisationProps) => {
         const real = Math.round(d.r * 10) / 10;
         const imaginary = Math.round(d.i * 10) / 10;
         tooltip.innerHTML = `${real}${imaginary < 0 ? "" : " + "}${imaginary}i`;
+      })
+      .on("mouseout", (e, d) => {
+        const tooltip = document.getElementById(tooltipId);
+        if (!tooltip) return;
+        tooltip.style.display = "none";
+      })
+      // add double click zoom in
+      .on("dblclick", (e, d) => {
+        const xRange = xExtent[1] - xExtent[0];
+        const yRange = yExtent[1] - yExtent[0];
+        const newXExtent: [number, number] = [
+          d.r - xRange / 4,
+          d.r + xRange / 4,
+        ];
+        const newYExtent: [number, number] = [
+          d.i - yRange / 4,
+          d.i + yRange / 4,
+        ];
+        setNewExtent(newXExtent, newYExtent);
       });
-  }, [svg, height, width, yExtent, xExtent, tooltipId]);
+  }, [
+    svg,
+    height,
+    width,
+    yExtent,
+    xExtent,
+    tooltipId,
+    yResolution,
+    xResolution,
+  ]);
 
-  return <div ref={ref}></div>;
+  const reset = () => {
+    setYExtent(initialYExtent);
+    setXExtent(initialXExtent);
+  };
+
+  const setNewExtent = (
+    newXExtent: [number, number],
+    newYExtent: [number, number]
+  ) => {
+    setYExtent(newYExtent);
+    setXExtent(newXExtent);
+  };
+
+  return (
+    <div ref={ref}>
+      <div className="absolute top-0 left-0">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={reset}
+        >
+          Reset
+        </button>
+        {/* select resolution */}
+        <select
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onChange={(e) =>
+            setResolution(e.target.value as keyof typeof resolutions)
+          }
+        >
+          {Object.keys(resolutions).map((resolution) => (
+            <option key={resolution} value={resolution}>
+              {resolution}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default Visualisation;
