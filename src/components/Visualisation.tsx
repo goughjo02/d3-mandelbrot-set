9555cf81b1825608236f504fa71e90ae6a980b56
Joey Gough <j.gough@novafutur.com>
2023-04-17 20:47:28 +0100

    use context and d3 range and scale to improve v isualisation

--- src/components/Visualisation.tsx
+++ src/components/Visualisation.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { scaleLinear, select } from "d3";
+import { range, scaleLinear, select } from "d3";
 import { useEffect, useMemo, useRef } from "react";
 
 type VisualisationProps = {};
@@ -10,8 +10,14 @@ type VisualisationProps = {};
  * y axis is the imaginary part of the complex number
  */
 
-const yExtent = [-1.2, -1];
+const yExtent = [-1.2, 0];
 const xExtent = [-0.9, 0.2];
+
+const xResolution = 0.01;
+const yResolution = 0.01;
+
+const yPoints = range(yExtent[0], yExtent[1], yResolution);
+const xPoints = range(xExtent[0], xExtent[1], xResolution);
 
 const mandelbrot = (c: { r: number; i: number }) => {
   let z = { x: 0, y: 0 };
@@ -33,68 +39,55 @@ const draw = ({
   height,
   width,
 }: {
-  el: SVGSVGElement;
+  el: HTMLCanvasElement;
   height: number;
   width: number;
 }) => {
-  const svg = select(el);
-  const pixelHeight = 1;
-  const pixelWidth = 1;
-  const numberX = Number(width) / pixelWidth;
-  const numberY = Number(height) / pixelHeight;
-  const rScale = scaleLinear().domain([0, width]).range(xExtent);
-  const iScale = scaleLinear().domain([0, height]).range(yExtent);
   const colorScale = scaleLinear().domain([0, 100]).range([0, 360]);
 
-  const dataForDisplay = Array.from({ length: numberX }, (_, xi) =>
-    Array.from({ length: numberY }, (_, xj) => {
+  const xScale = scaleLinear().domain(xExtent).range([0, width]);
+  const yScale = scaleLinear().domain(yExtent).range([0, height]);
+
+  const pixelHeight = height / yPoints.length;
+  const pixelWidth = width / xPoints.length;
+
+  const dataForDisplay = xPoints.map((r, xi) =>
+    yPoints.map((i, xj) => {
       const x = xi * pixelWidth;
       const y = xj * pixelHeight;
-      const r = rScale(x);
-      const i = iScale(y);
       return {
-        x,
-        y,
+        x: xScale(r),
+        y: yScale(i),
         n: mandelbrot({ r, i }),
       };
     })
   );
 
-  svg.selectAll("rect").remove();
-  svg
-    .selectAll("rect")
-    .data(dataForDisplay.flat())
-    .enter()
-    .append("rect")
-    .attr("x", (d) => d.x)
-    .attr("y", (d) => d.y)
-    .attr("width", pixelWidth)
-    .attr("height", pixelHeight)
-    .attr("fill", (d) => {
+  const ctx = el.getContext("2d");
+  if (!ctx) return;
+  ctx.clearRect(0, 0, width, height);
+  dataForDisplay.forEach((row) => {
+    row.forEach((d) => {
       if (d.n === 100) {
-        return "black";
+        return;
       }
       const hue = colorScale(d.n);
-      return `hsl(${hue}, 100%, 50%)`;
+      ctx.fillStyle = `hsl(${hue}, 100%, 50%)`;
+      ctx.fillRect(d.x, d.y, pixelWidth, pixelHeight);
     });
+  });
 };
 
 export const Visualisation = (_props: VisualisationProps) => {
-  const ref = useRef<SVGSVGElement>(null);
-  const width = useMemo(() => {
-    if (typeof window === "undefined") return 0;
-    return window.innerWidth;
-  }, []);
-  const height = useMemo(() => {
-    if (typeof window === "undefined") return 0;
-    return window.innerHeight;
-  }, []);
+  const ref = useRef<HTMLCanvasElement>(null);
+  const width = window.innerWidth;
+  const height = window.innerHeight;
   useEffect(() => {
     if (!ref.current) return;
     draw({ height, el: ref.current, width });
   }, [ref, width, height]);
   return (
-    <svg
+    <canvas
       className="border-red-100 border-4"
       height={height}
       width={width}
