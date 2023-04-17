f885169a7bfc5307eb425be2487c8729249c9d1f
Joey Gough <j.gough@novafutur.com>
2023-04-17 20:47:11 +0100

    dynamically load d3 visualisation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import { Inter } from "next/font/google";
-import Visualisation from "@src/components/Visualisation";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+const Visualisation = dynamic(() => import("@src/components/Visualisation"), {
+  ssr: false,
+});
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <Visualisation />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Visualisation />
+      </Suspense>
     </main>
   );
 }
